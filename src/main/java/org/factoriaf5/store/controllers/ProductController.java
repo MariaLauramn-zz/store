39a89290e73123318223120cee287090d2eae816
Marialauramn16 <hikco25@gmail.com>
2021-05-17 15:09:42 +0200

    agregando las funciones de productos y en el controlador

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,12 +1,30 @@
 package org.factoriaf5.store.controllers;
 
+import jdk.jfr.ContentType;
+import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-public class ProductController {
+
+public class ProductController<json, application> {
+
+    private List<Product> products;
+
+        public ProductController() {products = new ArrayList<>();}
+
     @GetMapping("/health")
-    public String Check() {
-        return "Hello! I'm a server.";
+    public List<Product> AllProduct() {return products;}
+
+    @PostMapping("/health")
+    public RedirectView addProduct(Product products) {
+        products.getName(products);
+        products.getPrice(products);
+        return new RedirectView("/");
     }
 }
---
+++ src/main/java/org/factoriaf5/store/model/Product.java
@@ -0,0 +1,20 @@
+package org.factoriaf5.store.model;
+
+public class Product {
+    private  String name;
+    private  int price;
+
+    public Product(String name,int price) {
+        this.name = name;
+        this.price= price;
+    }
+
+        public String getName(Product products) {
+        return name;
+    }
+
+    public int getPrice(Product products) {
+        return price;
+    }
+
+}
